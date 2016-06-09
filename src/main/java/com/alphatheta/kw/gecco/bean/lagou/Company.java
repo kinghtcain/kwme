158e363d249ecf06f7755e5658a42a3fe835f453
zhangzhiquan <191550636@qq.com>
2016-06-10 00:35:24 +0800

    在Job上增加职业发布者和公司的爬取信息，并且修改公司company的抓取信息

--- src/main/java/com/alphatheta/kw/gecco/bean/lagou/Company.java
+++ src/main/java/com/alphatheta/kw/gecco/bean/lagou/Company.java
@@ -1,7 +1,21 @@
 package com.alphatheta.kw.gecco.bean.lagou;
 
+import com.geccocrawler.gecco.annotation.Gecco;
+import com.geccocrawler.gecco.annotation.GeccoClass;
+import com.geccocrawler.gecco.annotation.Request;
+import com.geccocrawler.gecco.request.HttpRequest;
+import com.geccocrawler.gecco.spider.HtmlBean;
+
 /**
  * Created by zhangzhiquan on 2016/6/1.
  */
-public class Company {
+@Gecco(matchUrl = "http://www.lagou.com/gongsi/{companyId}.html",pipelines = {"consolePipeline",
+"companyPersistPipeline"})
+public class Company implements HtmlBean{
+
+    private static final long serialVersionUID = -377053120283382723L;
+
+    @Request
+    private HttpRequest request;
+
 }
--- src/main/java/com/alphatheta/kw/gecco/bean/lagou/Job.java
+++ src/main/java/com/alphatheta/kw/gecco/bean/lagou/Job.java
@@ -55,6 +55,49 @@ public class Job implements HtmlBean{
     @Html
     @HtmlField(cssPath="div.content_r dl.job_company dd div.work_addr,dl.job_company dd div:nth-child(0)")
     private String jobAddress;
+    //职位发布者及其处理简历信息
+    @Attr("class")
+    @HtmlField(cssPath = ".jd_publisher div.publisher_name a[title]")
+    private String jobPublisher;
+
+    //简历及处理信息数据
+    //1.简历及时处理率：投递后7天内处理完成的简历所占比例
+    @Text
+    @HtmlField(cssPath = ".publisher_data div:nth-child(1) span.data")
+    private String resumeDealRate;
+    @HtmlField(cssPath = ".publisher_data div:nth-child(1) span.tip")
+    private String resumeDealRateTip;
+    //2.简历处理用时:完成简历处理的平均用时
+    @Text
+    @HtmlField(cssPath = ".publisher_data div:nth-child(2) span.data")
+    private String resumeDealTime;
+    @HtmlField(cssPath = ".publisher_data div:nth-child(2) span.tip")
+    private String resumeDealTimeTip;
+
+    //公司信息
+    @HtmlField(cssPath = ".job_company dt a div")
+    private String companyName;
+    @Attr("alt")
+    @HtmlField(cssPath = ".job_company dt a img[alt]")
+    private String companyFullName;
+    //2.公司在拉勾上的页面
+    @Href
+    @HtmlField(cssPath = ".job_company dt a")
+    private String compnayLagouPage;
+    @Image(value = "src")
+    @HtmlField(cssPath = ".job_company dt a img")
+    private String titleImg;//公司在拉勾网上的图像
+
+    @HtmlField(cssPath = "job_company dd ul.c_feature:nth-child(1) li:nth-child(1)")
+    private String companyField;//公司所处领域
+    @HtmlField(cssPath = "job_company dd ul.c_feature:nth-child(1) li:nth-child(2)")
+    private String companyScale;//公司规模
+    @Href
+    @HtmlField(cssPath = "job_company dd ul.c_feature:nth-child(1) li:nth-child(3) a[href]")
+    private String companyHomePage;//公司自身的主页
+    @Html
+    @HtmlField(cssPath = "job_company dd ul.c_feature:nth-child(2) li")
+    private String companyDevStage;//公司发展阶段
 
     public HttpRequest getRequest() {
         return request;
@@ -132,17 +175,205 @@ public class Job implements HtmlBean{
     public void setSimilarJobLinks(List<String> similarJobLinks) {
         this.similarJobLinks = similarJobLinks;
     }
+
+    public String getJobPublisher() {
+        return jobPublisher;
+    }
+
+    public void setJobPublisher(String jobPublisher) {
+        this.jobPublisher = jobPublisher;
+    }
+
+    public String getResumeDealRateTip() {
+        return resumeDealRateTip;
+    }
+
+    public void setResumeDealRateTip(String resumeDealRateTip) {
+        this.resumeDealRateTip = resumeDealRateTip;
+    }
+
+    public String getResumeDealRate() {
+        return resumeDealRate;
+    }
+
+    public void setResumeDealRate(String resumeDealRate) {
+        this.resumeDealRate = resumeDealRate;
+    }
+
+    public String getResumeDealTime() {
+        return resumeDealTime;
+    }
+
+    public void setResumeDealTime(String resumeDealTime) {
+        this.resumeDealTime = resumeDealTime;
+    }
+
+    public String getResumeDealTimeTip() {
+        return resumeDealTimeTip;
+    }
+
+    public void setResumeDealTimeTip(String resumeDealTimeTip) {
+        this.resumeDealTimeTip = resumeDealTimeTip;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyFullName() {
+        return companyFullName;
+    }
+
+    public void setCompanyFullName(String companyFullName) {
+        this.companyFullName = companyFullName;
+    }
+
+    public String getCompnayLagouPage() {
+        return compnayLagouPage;
+    }
+
+    public void setCompnayLagouPage(String compnayLagouPage) {
+        this.compnayLagouPage = compnayLagouPage;
+    }
+
+    public String getTitleImg() {
+        return titleImg;
+    }
+
+    public void setTitleImg(String titleImg) {
+        this.titleImg = titleImg;
+    }
+
+    public String getCompanyField() {
+        return companyField;
+    }
+
+    public void setCompanyField(String companyField) {
+        this.companyField = companyField;
+    }
+
+    public String getCompanyScale() {
+        return companyScale;
+    }
+
+    public void setCompanyScale(String companyScale) {
+        this.companyScale = companyScale;
+    }
+
+    public String getCompanyHomePage() {
+        return companyHomePage;
+    }
+
+    public void setCompanyHomePage(String companyHomePage) {
+        this.companyHomePage = companyHomePage;
+    }
+
+    public String getCompanyDevStage() {
+        return companyDevStage;
+    }
+
+    public void setCompanyDevStage(String companyDevStage) {
+        this.companyDevStage = companyDevStage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Job job = (Job) o;
+
+        if (jobId != null ? !jobId.equals(job.jobId) : job.jobId != null) return false;
+        if (jobOfferName != null ? !jobOfferName.equals(job.jobOfferName) : job.jobOfferName != null) return false;
+        if (jobName != null ? !jobName.equals(job.jobName) : job.jobName != null) return false;
+        if (jobRequest != null ? !jobRequest.equals(job.jobRequest) : job.jobRequest != null) return false;
+        if (jobPayment != null ? !jobPayment.equals(job.jobPayment) : job.jobPayment != null) return false;
+        if (jobPublishTime != null ? !jobPublishTime.equals(job.jobPublishTime) : job.jobPublishTime != null)
+            return false;
+        if (jobDescription != null ? !jobDescription.equals(job.jobDescription) : job.jobDescription != null)
+            return false;
+        if (similarJobLinks != null ? !similarJobLinks.equals(job.similarJobLinks) : job.similarJobLinks != null)
+            return false;
+        if (jobAddress != null ? !jobAddress.equals(job.jobAddress) : job.jobAddress != null) return false;
+        if (jobPublisher != null ? !jobPublisher.equals(job.jobPublisher) : job.jobPublisher != null) return false;
+        if (resumeDealRate != null ? !resumeDealRate.equals(job.resumeDealRate) : job.resumeDealRate != null)
+            return false;
+        if (resumeDealRateTip != null ? !resumeDealRateTip.equals(job.resumeDealRateTip) : job.resumeDealRateTip != null)
+            return false;
+        if (resumeDealTime != null ? !resumeDealTime.equals(job.resumeDealTime) : job.resumeDealTime != null)
+            return false;
+        if (resumeDealTimeTip != null ? !resumeDealTimeTip.equals(job.resumeDealTimeTip) : job.resumeDealTimeTip != null)
+            return false;
+        if (companyName != null ? !companyName.equals(job.companyName) : job.companyName != null) return false;
+        if (companyFullName != null ? !companyFullName.equals(job.companyFullName) : job.companyFullName != null)
+            return false;
+        if (compnayLagouPage != null ? !compnayLagouPage.equals(job.compnayLagouPage) : job.compnayLagouPage != null)
+            return false;
+        if (titleImg != null ? !titleImg.equals(job.titleImg) : job.titleImg != null) return false;
+        if (companyField != null ? !companyField.equals(job.companyField) : job.companyField != null) return false;
+        if (companyScale != null ? !companyScale.equals(job.companyScale) : job.companyScale != null) return false;
+        if (companyHomePage != null ? !companyHomePage.equals(job.companyHomePage) : job.companyHomePage != null)
+            return false;
+        return !(companyDevStage != null ? !companyDevStage.equals(job.companyDevStage) : job.companyDevStage != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = jobId != null ? jobId.hashCode() : 0;
+        result = 31 * result + (jobOfferName != null ? jobOfferName.hashCode() : 0);
+        result = 31 * result + (jobName != null ? jobName.hashCode() : 0);
+        result = 31 * result + (jobRequest != null ? jobRequest.hashCode() : 0);
+        result = 31 * result + (jobPayment != null ? jobPayment.hashCode() : 0);
+        result = 31 * result + (jobPublishTime != null ? jobPublishTime.hashCode() : 0);
+        result = 31 * result + (jobDescription != null ? jobDescription.hashCode() : 0);
+        result = 31 * result + (similarJobLinks != null ? similarJobLinks.hashCode() : 0);
+        result = 31 * result + (jobAddress != null ? jobAddress.hashCode() : 0);
+        result = 31 * result + (jobPublisher != null ? jobPublisher.hashCode() : 0);
+        result = 31 * result + (resumeDealRate != null ? resumeDealRate.hashCode() : 0);
+        result = 31 * result + (resumeDealRateTip != null ? resumeDealRateTip.hashCode() : 0);
+        result = 31 * result + (resumeDealTime != null ? resumeDealTime.hashCode() : 0);
+        result = 31 * result + (resumeDealTimeTip != null ? resumeDealTimeTip.hashCode() : 0);
+        result = 31 * result + (companyName != null ? companyName.hashCode() : 0);
+        result = 31 * result + (companyFullName != null ? companyFullName.hashCode() : 0);
+        result = 31 * result + (compnayLagouPage != null ? compnayLagouPage.hashCode() : 0);
+        result = 31 * result + (titleImg != null ? titleImg.hashCode() : 0);
+        result = 31 * result + (companyField != null ? companyField.hashCode() : 0);
+        result = 31 * result + (companyScale != null ? companyScale.hashCode() : 0);
+        result = 31 * result + (companyHomePage != null ? companyHomePage.hashCode() : 0);
+        result = 31 * result + (companyDevStage != null ? companyDevStage.hashCode() : 0);
+        return result;
+    }
+
     @Override
     public String toString() {
         return "Job{" +
                 "jobId=" + jobId +
                 ", jobOfferName='" + jobOfferName + '\'' +
-                ", jobName='" + jobName + '\'' +
                 ", jobRequest='" + jobRequest + '\'' +
+                ", jobName='" + jobName + '\'' +
                 ", jobPayment='" + jobPayment + '\'' +
-                ", jobPublishTimeAndSite='" + jobPublishTime + '\'' +
+                ", jobPublishTime='" + jobPublishTime + '\'' +
                 ", jobDescription='" + jobDescription + '\'' +
+                ", similarJobLinks=" + similarJobLinks +
                 ", jobAddress='" + jobAddress + '\'' +
+                ", jobPublisher='" + jobPublisher + '\'' +
+                ", resumeDealRate='" + resumeDealRate + '\'' +
+                ", resumeDealRateTip='" + resumeDealRateTip + '\'' +
+                ", resumeDealTime='" + resumeDealTime + '\'' +
+                ", resumeDealTimeTip='" + resumeDealTimeTip + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", companyFullName='" + companyFullName + '\'' +
+                ", compnayLagouPage='" + compnayLagouPage + '\'' +
+                ", titleImg='" + titleImg + '\'' +
+                ", companyField='" + companyField + '\'' +
+                ", companyHomePage='" + companyHomePage + '\'' +
+                ", companyScale='" + companyScale + '\'' +
+                ", companyDevStage='" + companyDevStage + '\'' +
                 '}';
     }
 }
